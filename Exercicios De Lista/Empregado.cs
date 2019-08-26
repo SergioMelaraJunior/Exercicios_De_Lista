276beb7148f3aa20209df9cd539e013317fcc777
Sergio Melara Junior <sergio.melara@hotmail.com>
2019-08-26 12:01:19 -0300

    exercicios de lista

---
+++ Exercicios De Lista/Empregado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicios_De_Lista {
+    class Empregado {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Empregado(int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void IncreaseSalary(double percentage) {
+            Salario += Salario * percentage / 100.0;
+        }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+    
+
---
+++ Exercicios De Lista/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Exercicios_De_Lista {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Empregado> list = new List<Empregado>();
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Empregado #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Empregado(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Insira o ID do funcionário que terá aumento salarial : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Empregado emp = list.Find(x => x.Id == searchId);
+            if (emp != null) {
+                Console.Write("Entre com á porcentagem: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("Este ID não existe!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista de empregado atualizada:");
+            foreach (Empregado obj in list) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
